7d650e8d0691530e3f615318355df55f619ed96b
Mario Steingruber <mario.steingruber@rise-world.com>
2017-10-26 16:47:36 +0200

    experiments with fixed camera, works ok if the camera distance is fixed

---
+++ AR Tower Defense/Assets/sadlerSteingruberWayPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class sadlerSteingruberWayPoint : MonoBehaviour {
+
+	public TrackableBehaviour imageTarget;
+
+	public GameObject wayPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (imageTarget.CurrentStatus != TrackableBehaviour.Status.TRACKED) {
+			return;
+		}
+
+		wayPoint.transform.position = new Vector3 (imageTarget.transform.position.x, 0, imageTarget.transform.position.z);
+	}
+}
